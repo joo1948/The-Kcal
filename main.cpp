931fb3d5df9497009956bf9cc09d3e788d1092c4
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2019-12-03 20:55:08 +0900

    main

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@
 
 int main()
 {
+	
 	system("title The kcal");
-	system("mode con: cols=70 lines=30");//창 위치 조절(칸/행 ( 가로 ), 줄/열 ( 세로 )
+	system("mode con: cols=65 lines=30");//창 위치 조절(칸/행 ( 가로 ), 줄/열 ( 세로 )
 
 	TheKcal kcal(22, 15);
 	kcal.CursorView(0);
