86d1c3023840069529942c3f8e097a909b81a8a5
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2019-12-03 20:57:58 +0900

    헤더파일

--- TheKcal.h
+++ TheKcal.h
@@ -7,11 +7,16 @@
 #include <conio.h>
 #include <string>
 #include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
 using namespace std;
 
 #define UP 0
 #define DOWN 1
 #define SUBMIT 4
+
+
 
 class TheKcal{
 private:
@@ -19,6 +24,11 @@ private:
 	int y;
 	int ex_c;
 public:
+	void admire();//관리자
+	void showadmire(int k);//관리자 리뷰 보기 1
+	void showadmire_1();//관리자 리뷰 보기 2
+	void wirtememo();//관리자 메모하기
+	void readmemo();//관리자 메모 보기
 	TheKcal(int x, int y);
 	int keyControl();
 	void start2_display(void);
@@ -29,7 +39,10 @@ public:
 	void input();
 	int chooseSports();
 	void inputWantCal();
-	void ex_mathod();//무슨 운동했니?
+	int eatfood();//먹은 음식 뭐닝~~?
+	int savefoodKcal();//먹은 음식 칼로리 저장
+	void ex_mathod(int countcal);//무슨 운동했니?
+	void minus_kcal();//칼로리 소모한 양 보기
 	void CursorView(char show); //커서 숨기기
 	void textcolor(int color_number);//텍스트 컬러 조절
 	int key();//키 값 컨트롤하기
